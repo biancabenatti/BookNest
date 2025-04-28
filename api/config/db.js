c256ad9009cee5273e87c6050f57bc7b81f7ded0
Bianca <bianca071995@gmail.com>
2025-04-28 12:34:33 -0300

    alteração db.js

--- api/config/db.js
+++ api/config/db.js
@@ -2,11 +2,12 @@ import { MongoClient } from "mongodb";
 let db;
 export async function connectToDatabase(app){
     try{
-        const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/"
+        const MONGODB_URI = process.env.MONGODB_URI 
         const client = new MongoClient(MONGODB_URI)
         await client.connect()
         console.log(`Conectando ao MongoDB ${MONGODB_URI}!`)
-        db = client.db()
+        const dbName = process.env.DB_NAME || 'biblioteca'; 
+        db = client.db(dbName);
         //Disponibiliza o db globalmente no Express
         app.locals.db = db
         return db
@@ -15,7 +16,5 @@ export async function connectToDatabase(app){
         process.exit(1)
     }
 }
-
-
 
 export {db}
