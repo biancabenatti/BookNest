82655e53b7074913e2161643418f55bac18d4cb2
Bianca <bianca071995@gmail.com>
2025-06-10 11:55:10 -0300

    alteracao dotenv

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
 import { connectToDatabase } from './config/db.js'; // Apenas a função connectToDatabase
 import livrosRoutes from './routes/livros.js';
 import authRoutes from './routes/auth.js';
